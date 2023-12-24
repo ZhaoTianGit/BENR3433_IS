b01858b27dcb31f61f236254c50006f317025576
zhaotian <zhaotian.org@gmail.com>
2023-12-24 19:41:53 +0800

    4th swagger-login, showJWT

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 //const adminRouter = require('./route/admin');
+const jwt = require('jsonwebtoken');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const User = require('./model/user_schema')
@@ -36,6 +37,23 @@ const options = {
        title: 'BENR3433 INFORMATION SECURITY zt',
        version: '1.0.0',
      },
+     tags:[
+      { name: 'default', description: 'Default endpoints' },
+      { name: 'Admin', description: 'Admin API' },
+      { name: 'Host', description: 'Host API' },
+      { name: 'User', description: 'User API' }
+    ],
+    components: {
+      securitySchemes: {
+          Authorization: {
+              type: "http",
+              scheme: "bearer",
+              bearerFormat: "JWT",
+              value: "Bearer <JWT token here>",
+              description:"this is for authentication only, to log out, please use the logout api"
+          }
+        }
+      },
      servers:[
        {
            //url: 'https://isbenr3433.azurewebsites.net/'
@@ -49,29 +67,32 @@ const options = {
  const openapiSpecification = swaggerJsdoc(options);
  app.use('/swagger', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
-//admin routes
-//app.use('/admin',adminRouter);
-
-//test swagger
-/**
- * @swagger
- * /:
- *  get:
- *      summary: This api is for testing
- *      description: This api is used for testing
+ 
+ //test swagger
+ /**
+  * @swagger
+  * /:
+  *  get:
+  *      summary: This api is for testing
+  *      description: This api is used for testing
  *      responses:
  *          200:
  *              description: to test get api
  */
 app.get('/', (req, res) => {
-   res.send('Hello World! zt')
+  res.send('Hello World! zt')
 })
 
+//admin routes
+//app.use('/admin',adminRouter);
+
+//register swagger
 /**
-* @swagger
+ * @swagger
 * /register:
 *  post:
 *      summary: registration for new user
+*      tags: [User]
 *      description: this api fetch data from mongodb
 *      requestBody:
 *          required: true
@@ -88,10 +109,8 @@ app.get('/', (req, res) => {
 *                      type: object
 *                      properties:
 *                          user:
-*                              $ref: '#components/schema/User'
-*                          message:
-*                              type: string
-*                              description: Additional message
+*                              $ref: '#components/schema/registersuccessful'
+*                          
 *          409:
 *              description: Username has been taken
 *          500:
@@ -102,7 +121,7 @@ app.get('/', (req, res) => {
 *                       type: object
 *                       properties:
 *                          message:
-*                             ype: string
+*                             $ref: '#components/schema/errormessage'
 */
 
 //register new user
@@ -115,11 +134,11 @@ app.post('/register', async(req, res) => {
            username: username,
            password: password,
            name: name,
-           role: "user",
+           role: "User",
            login_status: false
          }  
          const user = await User.create(request)
-         const responsemessage= 'User registered successfully';
+         const responsemessage= 'User ' + username + ' registered successfully';
          res.status(200).json({user:user, message: responsemessage})}
        else{
            res.status(409).send('Username has been taken'); // 409 = there's a conflict that needs to be resolved before the request can be successfully processed
@@ -139,10 +158,37 @@ app.post('/register', async(req, res) => {
 *              properties:
 *                  username:
 *                      type: string
-*                  password:
+*                  password: 
 *                      type: string
 *                  name:
 *                      type: string
+*          registersuccessful:
+*              type: object
+*              properties:
+*                  username:
+*                      type: string
+*                  name:
+*                      type: string
+*                  message:
+*                      type: string
+*                      description: Additional message
+* 
+*          errormessage:
+*              type: object
+*              properties:
+*                message:
+*                  type: string
+*                  example: Internal server error occurred
+* 
+*          jwtinfo:
+*            type: object
+*            properties:
+*              username:
+*                type: string
+*              user_id: 
+*                type: string
+*              role:
+*                type: string
 * 
 *          User:
 *              type: object
@@ -150,17 +196,193 @@ app.post('/register', async(req, res) => {
 *                  username:
 *                      type: string
 *                  password:
-*                      type: string
+*                      type: string 
 *                  name:
 *                      type: string 
 *                  role:
-*                      type: string
+*                      type: string 
 *                  visitor_id:
-*                      type: string
+*                      type: string 
 *                      format: uuid
 *                  login_status:
 *                      type: boolean
 */
+
+//login swagger
+/**
+ * @swagger
+ *  /login:
+ *    post:
+ *      summary: Login for users
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *               type: object
+ *               properties:
+ *                 username:
+ *                  type: string
+ *                 password:
+ *                  type: string
+ *      responses:
+ *        200:
+ *          description: Successful login
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  username:
+ *                    type: string
+ *                    description: Username of the logged-in user
+ *                  message:
+ *                    type: string
+ *                    description: Login successful message
+ *                  accesstoken:
+ *                    type: string
+ *                    description: Generated access token for the logged-in user
+ *        401:
+ *          description: Unauthorized - Wrong password
+ *        404:
+ *          description: Username not found
+ *        409:
+ *          description: User is already logged in
+ *        500:
+ *          description: Internal server error
+ *          content:
+ *            application/json:
+ *              schema:
+ *                 $ref: '#components/schema/errormessage'
+ *                
+ */
+
+app.post('/login',async(req,res)=>{
+  const {username,password}=req.body
+  try {
+    const b = await User.findOne({username:req.body.username})
+    if(b==null){
+      res.status(404).send('Username not found');
+    }else{
+      if(b.login_status==true){
+        res.status(409).send('User is already logged in');
+      }else{
+        const c = req.body.password === b.password;      
+        if(!c){
+          res.status(401).send('Unauthorized: Wrong password');
+        }else{
+        await User.updateOne({username:req.body.username},{$set:{login_status:true}})
+        const login_user= await User.findOne({username:req.body.username})
+        access_token=jwt.sign({username:login_user.username,user_id:login_user._id,role:login_user.role},process.env.JWT_SECRET)
+        res.json({username:login_user.username,message:"login successful",accesstoken: access_token})
+      }
+      }
+      }}
+   catch (error) {
+    console.log(error.message);
+        res.status(500).json({message: error.message})
+  }
+})
+
+//middleware
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, login_user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.user = login_user
+    next()
+  })
+}
+
+/**
+ * @swagger
+ *  /showjwt:
+ *    get:
+ *      summary: Display user information from JWT token
+ *      security:
+ *        - Authorization: []
+ *      responses:
+ *        200:
+ *          description: Successful retrieval of user information
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#components/schema/jwtinfo'
+ *                description: User information retrieved from JWT token
+ *        401:
+ *          description: Unauthorized - Invalid or missing token
+ *          
+ */
+//test jwt
+app.get('/showjwt',authenticateToken,(req,res)=>{
+  res.send(req.user)
+})
+
+/**
+ * @swagger
+ *  components:
+ *      schema:
+ *          registerinfo:
+ *              type: object
+ *              properties:
+ *                  username:
+ *                      type: string
+ *                  password:
+ *                      type: string
+ *                  name:
+ *                      type: string
+ * 
+ * 
+ *          registersuccessful:
+ *              type: object
+ *              properties:
+ *                  username:
+ *                      type: string
+ *                  name:
+ *                      type: string
+ *                  message:
+ *                      type: string
+ *                      description: Additional message
+ * 
+ *          errormessage:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Internal server error occurred
+ * 
+ *          jwtinfo:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *              user_id: 
+ *                type: string
+ *              role:
+ *                type: string
+ * 
+ *          User:
+ *              type: object
+ *              properties:
+ *                  username:
+ *                      type: string
+ *                  password:
+ *                      type: string
+ *                  name:
+ *                      type: string 
+ *                  role:
+ *                      type: string
+ *                  visitor_id:
+ *                      type: string
+ *                      format: uuid
+ *                  login_status:
+ *                      type: boolean
+ */
+
 
 //admin create new host account
 // authenticatedhost to see all created visitor
